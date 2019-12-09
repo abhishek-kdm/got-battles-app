ceee9680b7072b163ab4afbf1092e1bc45c3ddee
Abhishek <abhishekk19@gmail.com>
2019-12-10 00:22:23 +0530

    server refactoring, 404 page.

--- server/index.js
+++ server/index.js
@@ -14,22 +14,43 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 5000;
 const MONGOOSE_URI = process.env.MONGO_ATLAS_URI;
 
+
 // mongo stuff.
-connect(MONGOOSE_URI, { useNewUrlParser: true, useCreateIndex: true });
+connect(MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 connection.once('open', () => {
   console.log('MongoDB  connection established successfully.');
 });
 
 
+
 // express stuff.
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+// using the default create react app dev server
+// during non-production use.
+if (process.env.NODE_ENV === 'production') {
+  const PUBLIC_PATH = resolve(__dirname, '..', 'build');
+
+  app.use(express.static(resolve(PUBLIC_PATH)));
+
+  app.get('/', (_, res) => {
+    res.sendFile(resolve(PUBLIC_PATH, 'index.html'));
+  });
+}
 
 // routes
 app.use('/battle', BattleRouter);
-
+// 404.
+app.get('*', (_, res) => {
+  res.sendFile(resolve(__dirname, '404.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
