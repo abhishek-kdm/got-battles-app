736fad65aca6040c4ca85ceaff1b24e49967c827
Abhishek Kadam <abhishekk19@gmail.com>
2021-06-26 03:49:44 +0530

    Workflow updated (using custom personal deploy action).

--- scripts/aws_ec2_deploy.sh
+++
@@ -1,5 +0,0 @@
-rsync -avP --delete-excluded \
-  --include="*.html" --include="*.webmanifest" --exclude=".git*" --include="*/" \
-  --exclude="*" --rsync-path="mkdir -p $WEB_PATH/$REPO_NAME && rsync" \
-  -e "ssh -o StrictHostKeyChecking=no" $1 $2
-
